f50bddfcffd348f722ec0b9493c0665631c25bbf
Manjunath Yaji <manuyaji@gmail.com>
2019-09-17 15:31:28 +0530

    HMAC generator added

---
+++ hmac-generator/hmacgenerator/src/main/java/com/yaji/hmacgenerator/hmacgenerator/App.java
@@ -0,0 +1,13 @@
+package com.yaji.hmacgenerator.hmacgenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
